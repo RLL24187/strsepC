ba58c2355762a170fbcf07c57f4cab207fd34788
rleongPd5 <rleong00@stuy.edu>
2019-11-26 15:21:54 -0500

    Added headers

---
+++ parse.c
@@ -0,0 +1,9 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <dirent.h>
